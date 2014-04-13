cdbc0c02f81d8148b0969f6d7229768a5ebe3f02
ncaq <ncaq@ncaq.net>
2014-04-14 00:25:03 +0900

    排他制御を設定したらだいぶマシになったが,根本的に1回ズレたら収拾がつかない

--- haskell-echo-type.el
+++ haskell-echo-type.el
@@ -5,16 +5,16 @@
 (defvar-local haskell-echo-type/output-string "")
 
 (defun haskell-echo-type/setup ()
-  (let ((process-connection-type nil));pipe
-    (setq haskell-echo-type/process (start-process "haskell-echo-type" nil "ghci" (buffer-file-name)))
-    (set-process-filter haskell-echo-type/process 'haskell-echo-type/output-filter)
-    (set-process-query-on-exit-flag haskell-echo-type/process nil)))
+  (setq haskell-echo-type/process (start-process "haskell-echo-type" nil "ghci" (buffer-file-name)))
+  (set-process-filter haskell-echo-type/process 'haskell-echo-type/output-filter)
+  (set-process-query-on-exit-flag haskell-echo-type/process nil))
 
 (defun haskell-echo-type/reload ()
   (process-send-string haskell-echo-type/process (concat ":reload " (buffer-file-name)))
   (process-send-string haskell-echo-type/process "\n"))
 
 (defun haskell-echo-type/output-filter (process source)
+  ;; (setq haskell-echo-type/output-string (concat haskell-echo-type/output-string source)))
   (setq haskell-echo-type/output-string (car (split-string source "\n"))))
 
 (defun add-bracket-symbol (source)
@@ -26,12 +26,10 @@
 (defun haskell-echo-type ()
   (interactive)
   (let ((source (thing-at-point 'word)))
-    (if source
-        (progn (process-send-string haskell-echo-type/process (concat ":t " (add-bracket-symbol source)))
-               (process-send-string haskell-echo-type/process "\n")
-               (accept-process-output haskell-echo-type/process 1)
-               (message haskell-echo-type/output-string))
-      ())))
+    (process-send-string haskell-echo-type/process (concat ":t " (add-bracket-symbol source)))
+    (process-send-string haskell-echo-type/process "\n")
+    (accept-process-output haskell-echo-type/process 10 0 t)
+    (message haskell-echo-type/output-string)))
 
 (defun turn-on-haskell-echo-type ()
   (interactive)
