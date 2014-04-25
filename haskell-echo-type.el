30aecab35e5899860e010941a29aec28fb8a4da6
ncaq <ncaq@ncaq.net>
2014-04-25 21:41:09 +0900

    turn-offを追加する 一時的に無効にしたい時とか,あるもんね

--- haskell-echo-type.el
+++ haskell-echo-type.el
@@ -4,13 +4,24 @@
 (require 'thingatpt)
 
 (defvar-local haskell-echo-type/process nil)
-(defvar-local haskell-echo-type/queue "")
+(defvar-local haskell-echo-type/queue nil)
 
 (defun turn-on-haskell-echo-type ()
   (interactive)
   (haskell-echo-type/setup)
   (add-hook 'after-save-hook 'haskell-echo-type/reload nil t)
+  (add-hook 'kill-buffer-hook 'haskell-echo-type/kill nil t)
   (add-hook 'post-command-hook 'haskell-echo-type nil t))
+
+(defun turn-off-haskell-echo-type ()
+  (interactive)
+  (if haskell-echo-type/process
+      (haskell-echo-type/kill))
+  (remove-hook 'after-save-hook 'haskell-echo-type/reload t)
+  (remove-hook 'kill-buffer-hook 'haskell-echo-type/kill t)
+  (remove-hook 'post-command-hook 'haskell-echo-type t)
+  (setq haskell-echo-type/process nil)
+  (setq haskell-echo-type/queue nil))
 
 (defun haskell-echo-type/setup ()
   (setq haskell-echo-type/process (start-process "haskell-echo-type" nil "ghci" (buffer-file-name)))
@@ -21,6 +32,10 @@
 
 (defun haskell-echo-type/reload ()
   (process-send-string haskell-echo-type/process (concat ":reload " (buffer-file-name)))
+  (process-send-string haskell-echo-type/process "\n"))
+
+(defun haskell-echo-type/kill ()
+  (process-send-string haskell-echo-type/process ":quit")
   (process-send-string haskell-echo-type/process "\n"))
 
 (defun haskell-echo-type ()
@@ -40,15 +55,15 @@
 (defun haskell-echo-type/output-filter (process source)
   (setq haskell-echo-type/queue (concat haskell-echo-type/queue source))
   (let ((source-list (split-string haskell-echo-type/queue "\n")))
-    (when (and (haskell-echo-type/success? source-list))
-      (haskell-echo-type/success-output source-list))))
+    (if (and (haskell-echo-type/success? source-list))
+        (let ((output (car (last source-list 2))))
+          (haskell-echo-type/success-output output)))))
 
 (defun haskell-echo-type/success? (source-list)
   (and (string= (car (last source-list)) " ")
        (string-match "^[^\W].+ :: .*[A-Z].*" (car (last source-list 2)))))
 
-(defun haskell-echo-type/success-output (source-list)
-  (let ((message (s-trim (car (last source-list 2)))))
-    (minibuffer-message message)))
+(defun haskell-echo-type/success-output (output)
+  (message output))
 
 (provide 'haskell-echo-type)
